31ab168cf12bf0f673ed2543daf2eaed329bd446
FlashCat <FlashCat@users.noreply.github.com>
2016-08-10 15:08:23 +0000

    rebuild pages at be0583c

--- implementors/compare/trait.Compare.js
+++ implementors/compare/trait.Compare.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['compare'] = [];
+implementors["compare"] = [];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
